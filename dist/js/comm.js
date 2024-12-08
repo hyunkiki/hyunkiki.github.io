a0062227c5cc0cabe94efa4f5860a8275d21fe9f
hyunkiki <rlagusrl98@gmail.com>
2024-12-08 22:19:43 +0900

    12-08 업데이트

--- dist/js/comm.js
+++ dist/js/comm.js
@@ -26,6 +26,23 @@ Intromove.to([".welcome_wr .welcome_txt01", ".welcome_wr .welcome_txt02"], {
   ease: "bounce.out",
 }); //intro부분
 
+//skill 부분
+gsap.registerPlugin(ScrollTrigger);
+ScrollTrigger.create({
+  trigger: "#main #skill",
+  start: "top top",
+  end: "bottom bottom",
+  pin: "#main #skill .left",
+  scrub: 1,
+
+  onEnter: function () {
+    $("#header").addClass("section2");
+  },
+  onLeaveBack: function () {
+    $("#header").removeClass("section2");
+  },
+});
+
 var swiper = new Swiper(".mySwiper", {
   navigation: {
     nextEl: ".swiper-button-next",
