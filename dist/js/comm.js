0d961ef32e83ab49a7168990827eae5ce17616e2
hyunkiki <rlagusrl98@gmail.com>
2025-04-04 16:56:26 +0900

    04-04 업데이트

--- dist/js/comm.js
+++ dist/js/comm.js
@@ -1,6 +1,6 @@
 gsap.registerPlugin(ScrollTrigger);
 
-// 1. 인트로 애니메이션
+// 1. intro 애니메이션
 const Intromove = gsap.timeline();
 Intromove.to(".welcome_wr .welcome_img", {
   duration: 2,
@@ -15,7 +15,7 @@ Intromove.to([".welcome_wr .welcome_txt01", ".welcome_wr .welcome_txt02"], {
   ease: "bounce.out",
 });
 
-// 2. 프로필 섹션 애니메이션 (DOMContentLoaded 후 실행)
+// 2. Profile 섹션 애니메이션 (DOMContentLoaded 후 실행)
 document.addEventListener("DOMContentLoaded", () => {
   const tl = gsap.timeline({
     scrollTrigger: {
@@ -40,9 +40,146 @@ document.addEventListener("DOMContentLoaded", () => {
   );
 });
 
-// 3. 스킬 섹션 ScrollTrigger 설정 함수
+// 3. Skill 섹션
 
-// 4. SNS 디자인 Swiper 설정
+// 4. BANNER 디자인
+// banner tit
+const btl = gsap.timeline({
+  scrollTrigger: {
+    trigger: ".banner_tit",
+    start: "top 100%",
+    end: "bottom 90%",
+    scrub: 1,
+    markers: true,
+  },
+});
+btl.fromTo(
+  ".letter",
+  { y: 20, opacity: 0 },
+  { y: 0, opacity: 1, duration: 3, ease: "power2.out", stagger: 0.15 }
+);
+
+// box1
+const tl1 = gsap.timeline({
+  scrollTrigger: {
+    trigger: ".banner_img_wr .box1",
+    start: "top 100%",
+    end: "bottom 90%",
+    scrub: 1,
+    markers: false,
+  },
+});
+tl1.fromTo(
+  ".ban1",
+  { clipPath: "polygon(50% 50%, 50% 50%, 50% 50%, 50% 50%)" },
+  {
+    clipPath: "polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)",
+    duration: 4,
+    ease: "power2.out",
+  },
+  0
+);
+tl1.fromTo(
+  ".ban_tit1",
+  { opacity: 0, y: "70%" },
+  {
+    opacity: 1,
+    y: "0",
+    duration: 3,
+  }
+);
+
+// box2
+const tl2 = gsap.timeline({
+  scrollTrigger: {
+    trigger: ".banner_img_wr .box2",
+    start: "top 100%",
+    end: "bottom 90%",
+    scrub: 1,
+    markers: false,
+  },
+});
+tl2.fromTo(
+  ".ban2",
+  { clipPath: "polygon(50% 50%, 50% 50%, 50% 50%, 50% 50%)" },
+  {
+    clipPath: "polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)",
+    duration: 4,
+    ease: "power2.out",
+  },
+  0
+);
+tl2.fromTo(
+  ".ban_tit2",
+  { opacity: 0, y: "70%" },
+  {
+    opacity: 1,
+    y: "0",
+    duration: 3,
+  }
+);
+
+// box3
+const tl3 = gsap.timeline({
+  scrollTrigger: {
+    trigger: ".banner_img_wr .box3",
+    start: "top 100%",
+    end: "bottom 90%",
+    scrub: 1,
+    markers: false,
+  },
+});
+tl3.fromTo(
+  ".ban3",
+  { clipPath: "polygon(50% 50%, 50% 50%, 50% 50%, 50% 50%)" },
+  {
+    clipPath: "polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)",
+    duration: 4,
+    ease: "power2.out",
+  },
+  0
+);
+tl3.fromTo(
+  ".ban_tit3",
+  { opacity: 0, y: "70%" },
+  {
+    opacity: 1,
+    y: "0",
+    duration: 3,
+  }
+);
+
+// box4
+const tl4 = gsap.timeline({
+  scrollTrigger: {
+    trigger: ".banner_img_wr .box4",
+    start: "top 100%",
+    end: "bottom 90%",
+    scrub: 1,
+    markers: false,
+  },
+});
+tl4.fromTo(
+  ".ban4",
+  { clipPath: "polygon(50% 50%, 50% 50%, 50% 50%, 50% 50%)" },
+  {
+    clipPath: "polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)",
+    duration: 4,
+    ease: "power2.out",
+  },
+  0
+);
+tl4.fromTo(
+  ".ban_tit4",
+  { opacity: 0, y: "70%" },
+  {
+    opacity: 1,
+    y: "0",
+    duration: 3,
+  }
+);
+
+// 5. SNS 디자인
 var swiper = new Swiper(".mySwiper", {
   slidesPerView: 1,
   spaceBetween: 30,
@@ -56,7 +193,7 @@ var swiper = new Swiper(".mySwiper", {
     1024: { slidesPerView: 3, spaceBetween: 30 },
   },
 });
-// 5. 프로젝트 섹션 가로 스크롤 애니메이션
+// 6. Project 섹션
 const horizontalSection = document.querySelector(".horizontal_section");
 const wrapper = horizontalSection.querySelector(".wrapper");
 const items = wrapper.querySelectorAll(".item");
